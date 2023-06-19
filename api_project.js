6fab3c23014550c69663bb34a8b6b6aa704d63f7
Arnak <arnak@melikyan.am>
2023-06-19 12:39:21 +0400

    first commit

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Health",
+  "version": "1.0.0",
+  "description": "API документация",
+  "title": "Проект Health",
+  "url": "https://dev-api-health.jsay.ru/api/v1/",
+  "sampleUrl": true,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2023-06-19T10:53:09.716ZY",
+    "url": "https://apidocjs.com",
+    "version": "0.28.1"
+  }
+});
